c2fa79ad87ac06eadbcb838345a156c098a7b2d0
Алексей <elevanbass@gmail.com>
2013-02-17 12:51:49 +0200

    Оригинальный Index.php

--- index.php
+++ index.php
@@ -1,28 +1,8 @@
 <?
-session_start();
 #Главная Страница
 #CMS ZephCMS maded by ZephyR http://vk.com/el9vanbys
 #Если не знаешь - не лезь!
+session_start();
+require_once('def.php');
+$tpl->parse();
 ?>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
-</head>
-<body>
-<?
-if(!empty($_SESSION['login'])){
-?>
-<form method='post' action='posts/auth_post.php'>
-Логин : </br><input type='text' name='login'></br>
-Пароль : </br><input type='password' name='password'></br>
-<input type='submit' name='submit' value='Войти'>
-</form>
-<?
-}else{
-  echo "Привет,$_SESSION[login] .";
-}
-}
-?>
-</body>
-</html>
-</html>
