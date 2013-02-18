9e665c26a727083f969ef5f7f500df17ec39a084
Алексей <elevanbass@gmail.com>
2013-02-18 18:29:10 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,5 +4,13 @@
 #Если не знаешь - не лезь!
 session_start();
 require_once('def.php');
+$resultat = mysql_query("SELECT * FROM `news`");
+$news = array();
+while($myress = mysql_fetch_array($resultat)){
+//$tpl->set('{content}', $myress['content']);
+//$tpl->set('{title}', $myress['title']);
+$news[ $myress['id'] ] = array( $myress['author'],  $myress['date'] ,  $myress['title'], $myress['content'], );
+}
+$tpl->set('{news}', $news); 
 $tpl->parse();
 ?>
