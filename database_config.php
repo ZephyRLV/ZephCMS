9256712eff28adc238b26714eb196ccaa51845b0
Roman <zabaluevr@gmail.com>
2013-02-24 16:30:44 +0300

    Update database_config.php
    Омг. оптимизация, не?

--- database_config.php
+++ database_config.php
@@ -3,10 +3,9 @@ $db_host = 'localhost';
 $db_name = 'ZephCMS';
 $db_user = 'root';
 $db_password = '';
-$db_connect = mysql_connect($db_host,$db_user,$db_password);
-mysql_select_db($db_name,$db_connect);
-if(!$db_connect){
-echo "Cant Connect to BD";
-exit();
-}
+if (!mysql_connect($db_host, $db_user, $db_password)||!mysql_select_db($db_name)) { die("Mysql connection error."); }
+/*if(!$db_connect){
+echo "Can`t Connect to DB";
+die("Can`t Connect to DB");
+}*/
 ?>
